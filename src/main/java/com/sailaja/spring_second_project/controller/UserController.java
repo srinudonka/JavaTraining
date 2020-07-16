636d0b07274775002ebbdb0fba41ad0dc2439b4d
SailajaPraveen <SailajaPraveen@lakshmisailajachalla>
2020-07-16 11:18:00 -0500

    added city controller

--- src/main/java/com/sailaja/spring_second_project/controller/UserController.java
+++ src/main/java/com/sailaja/spring_second_project/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
